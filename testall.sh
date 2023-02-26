885d468ffdc8e1c2b07259ac39f2e1589b9b4043
David Buchanan <d@vidbuchanan.co.uk>
2023-02-26 16:55:51 +0000

    Add test script, rework race-avoidance

---
+++ testall.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo
+echo
+echo === syscall-static-pie ===
+python3 elf_to_stelf.py test_elfs/syscall-static-pie out.sh -r
+echo
+
+echo sh:
+sh ./out.sh
+echo
+echo ash:
+ash ./out.sh
+echo
+echo bash:
+bash ./out.sh
+echo
+echo dash:
+dash ./out.sh
+echo
+echo zsh:
+zsh ./out.sh
+echo
+
+echo
+echo === syscall-static-pie ===
+python3 elf_to_stelf.py test_elfs/glibc-static-pie out.sh -a hello -a world
+echo
+
+echo sh:
+sh ./out.sh
+echo
+echo ash:
+ash ./out.sh
+echo
+echo bash:
+bash ./out.sh
+echo
+echo dash:
+dash ./out.sh
+echo
+echo zsh:
+zsh ./out.sh
+echo
